5aad67b661b5abcc164bcb02d5cc03f17c806848
valhayot <valeriehayot@gmail.com>
2020-02-29 00:39:48 -0500

    some cleanup

--- boutiques2pydra.py
+++ boutiques2pydra.py
@@ -11,25 +11,7 @@ import sys
 from os import path as op
 
 
-def descriptor_parser(descriptor):
-
-    tool = loadJson(descriptor)
-    
-    parser = argparse.ArgumentParser(tool['name'], description=tool['description'])
-
-    type_mappings = { 'String': str, 'File': str, 'Flag': 'store_true', 'Number': float }
-
-    for in_arg in tool['inputs']:
-        if not in_arg['optional']:
-            parser.add_argument(in_arg['id'], type=type_mappings[in_arg['type']], help=in_arg['description'])
-        else:
-            parser.add_argument(in_arg['command-line-flag'], "--{}".format(in_arg['id']), action=type_mappings[in_arg['type']], help=in_arg['description'])
-
-    return parser
-
-
 class Boutiques2Pydra:
-
     def __init__(self, descriptor, *args, input_spec=None):
         desc_dict = loadJson(descriptor)
         desc_json = dumps(desc_dict)
@@ -38,7 +20,9 @@ class Boutiques2Pydra:
             invocation = example(desc_json)
         else:
             invocation = example(desc_json, "-c")
-            invocation = dumps({k:v for k,v in loads(invocation).items() if k in input_spec})
+            invocation = dumps(
+                {k: v for k, v in loads(invocation).items() if k in input_spec}
+            )
             print(invocation)
 
         tool_outputs = evaluate(desc_json, invocation, "output-files")
@@ -47,7 +31,7 @@ class Boutiques2Pydra:
 
         parameters = ", ".join([k for k in loads(invocation).keys()])
 
-        self.pydra_task = r'''def pydra_task({0}):
+        self.pydra_task = r"""def pydra_task({0}):
 
             kwargs = locals()
             tool = function(dumps({1}))
@@ -56,45 +40,15 @@ class Boutiques2Pydra:
             if ret.exit_code != 0:
                 raise Exception(ret.stdout, ret.stderr)
             
-            return [out_f.file_name.encode("utf-8") for out_f in ret.output_files]'''.format(parameters, desc_dict, list(args))
+            return [out_f.file_name.encode("utf-8") for out_f in ret.output_files]""".format(
+            parameters, desc_dict, list(args)
+        )
 
         code = compile(self.pydra_task, "<string>", "exec")
 
         self.create_task = FunctionType(code.co_consts[0], globals())
 
-        self.create_task = pydra.mark.annotate({ "return" : {"outfile": ty.Any} })(self.create_task)
+        self.create_task = pydra.mark.annotate({"return": {"outfile": ty.Any}})(
+            self.create_task
+        )
         self.create_task = pydra.mark.task(self.create_task)
-
-
-"""
-@pydra.mark
-def create_task(descriptor, args, **kwargs):
-    desc_json = dumps(loadJson(descriptor))
-
-    invocation = example(desc_json)
-    
-    #tool_inputs = evaluate(desc_json, invocation, "inputs")
-    tool_outputs = evaluate(desc_json, invocation, "output-files")
-
-    output_types = {k: ty.Any for k in tool_outputs.keys()}
-
-    #self = pydra.mark.annotate({ "return" : output_types })(self)
-    def pydra_tool(args, kwargs):
-        print(kwargs)
-        tool = function(desc_json)
-        ret = tool(*args, **kwargs)
-
-        if ret.exit_code != 0:
-            raise Exception(ret.stdout, ret.stderr)
-        
-        print(ret.output_files)
-        return [out_f for out_f in ret.output_files]
-
-
-    print(kwargs)
-    task = pydra_tool(args, kwargs)
-
-    return task
-"""
-
-#print(boutiques2pydra("zenodo.3267250", infile="test.nii", maskfile="test"))
